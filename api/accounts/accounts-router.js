1127f9089b9eaaeb73e48c464c05055d0c75f738
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-21 15:52:26 -0700

    All posts renders to the screen

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,7 +1,18 @@
+
+const express = require('express')
+const Accounts = require('./accounts-model')
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+Accounts.getAll()
+  .then((accounts) => {
+    res.status(200).json(accounts)
+  })
+  .catch((err) => {
+    res.status(500).json({
+      message: 'Cannot get Accounts'
+    })
+  })
 })
 
 router.get('/:id', (req, res, next) => {
